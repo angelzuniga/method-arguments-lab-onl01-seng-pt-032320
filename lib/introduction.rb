44288d3ac95989e3cd46ce837a7925aa371437cc
Angel Zuniga <angelzuniga@outlook.com>
2020-03-26 06:46:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,11 @@ def introduction(name)
   puts "Hello, #{name}!"
 end
 
+introduction("Angel")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Angel", "Ruby")
+
